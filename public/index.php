6967e18d2ff4ce7a8b533ddc2af5822ccca8b730
matsuba51 <GitHubmk.21s@rakumail.jp>
2025-04-25 15:25:41 +0900

    公開パスを調整する

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->bind('path.public', function() {
+    return __DIR__;
+});
+
+$app->handleRequest(Request::capture());
